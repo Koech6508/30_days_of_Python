6d974c81625f8cd0cc05a74632c9c4850dd4bf2b
Koech debby <99505478+Koech6508@users.noreply.github.com>
2023-11-18 18:17:02 +0000

    operators command

---
+++ day_3/operators.py
@@ -0,0 +1,12 @@
+age = 21 #declaring age as a variable
+print(age)
+height = 5.0 #declaring height as a variable
+print(height)
+comp = 3 +4j #declaring a variable that stores complex number
+print(comp)
+
+side_a = float(input('Enter side a: '))
+side_b = float(input('Enter side b: '))
+side_c = float(input('Enter side c: '))
+perimeter = side_a+side_b+side_c
+print(f'The perimeter is {perimeter}')
